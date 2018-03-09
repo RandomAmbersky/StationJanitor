8146afa452ee0e3c56589996e28b3ac29c2d1ea4
S.Reisinger <s.reisinger@informita.lt>
2018-03-09 08:55:39 +0100

    - Latest build / DropBox

--- StationJanitor/Properties/AssemblyInfo.cs
+++ StationJanitor/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.18058.2000")]
-[assembly: AssemblyFileVersion("1.0.18058.2000")]
+[assembly: AssemblyVersion("1.0.18068.0755")]
+[assembly: AssemblyFileVersion("1.0.18068.0755")]
